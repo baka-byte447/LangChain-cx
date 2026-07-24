98005ad6abd136a78e0527f43bf57148eaeb9904
baka-byte447 <milansharma2808@gmail.com>
2026-07-24 12:58:11 +0530

    embedding models

--- Lanchain-models/2-Chat-Models/huggingface_chatmodel.py
+++ Lanchain-models/2-Chat-Models/huggingface_chatmodel.py
@@ -8,10 +8,11 @@ hf_token = os.getenv("HUGGINGFACEHUB_ACCESS_TOKEN")
 llm = HuggingFaceEndpoint(
     repo_id="Qwen/Qwen2.5-7B-Instruct",
     task="text-generation",
-    huggingfacehub_api_token = hf_token
+    huggingfacehub_api_token = hf_token,
+    temperature=1
 )
 
 model = ChatHuggingFace(llm=llm)
 
-result = model.invoke("What is the capital of Cabo Verde?")
+result = model.invoke("Write a 5 line Dark Souls themed poetry")
 print(result.content)
